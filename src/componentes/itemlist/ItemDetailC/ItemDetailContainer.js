44fa2e5f262b04c3c7c23a12a1d9a9d27b06f2b4
matias <matiasangelaccio03@gmail.com>
2022-01-11 18:32:00 -0300

    correccion

--- src/componentes/itemlist/ItemDetailC/ItemDetailContainer.js
+++ src/componentes/itemlist/ItemDetailC/ItemDetailContainer.js
@@ -4,17 +4,22 @@ import { getFetch } from '../Mock'
 import ItemDetail from '../ItemDetailC/ItemDetail'
 import { useParams } from 'react-router-dom'
 const ItemDetailContainer = () => {
-const [producto, setproducto] = useState({})
+const [producto, setProducto] = useState({})
+const [loading, setLoading] = useState(true);
 const{idDetalle} = useParams()
 useEffect(()=>{
 getFetch 
-.then(resp=>setproducto(resp.find(prod =>prod.id === idDetalle)))
-},[])
+.then(resp=>{
+setProducto(resp.find(prod =>parseInt(prod.id) === parseInt(idDetalle)))
+setLoading(false);
+})
+},[idDetalle])
 
     return (
         <div>
-            
-            <ItemDetail  producto={producto}  />
+          {loading ? 'cargando' :  
+            <ItemDetail  producto={producto} />
+            }
         </div>
     )
 }
