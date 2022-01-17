2140865a818040d714266f417c2910dea20ef3e3
matias <matiasangelaccio03@gmail.com>
2022-01-17 16:08:12 -0300

    desafio events

--- src/componentes/itemlist/ItemCount.js
+++ src/componentes/itemlist/ItemCount.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import './ItemCount.css'
 
-const ItemCount = ({minimo, maximo, onAdd}) => {
+const ItemCount = ({ minimo, maximo, onAdd }) => {
     const [count, setCount] = useState(minimo)
     const handleCount = () => {
         (count < maximo) && setCount(prev => prev + 1)
@@ -12,18 +12,21 @@ const ItemCount = ({minimo, maximo, onAdd}) => {
         // }
     }
     const removeCount = () => {
-         (count > minimo) ? setCount(prev => prev - 1) : alert("minimo de compra alcanzado")
-    
+        (count > minimo) ? setCount(prev => prev - 1) : alert("minimo de compra alcanzado")
+
     }
     return (
         <div className='itemCount'>
 
             <button className='buttonItemCount' onClick={removeCount}>-</button>
            <h3 className='count'>{count}</h3> 
+
             <button  className='buttonItemCount'  onClick={handleCount}>+</button>
-     
-            
-        </div>
+                   
+                <div>  
+            <a href="/"><button onClick={() => onAdd(count)} className='bn632-hover bn25'>Agregar al carrito</button></a> 
+            </div> 
+ </div>
     )
 }
 
--- src/componentes/itemlist/ItemDetailC/ItemDetail.js
+++ src/componentes/itemlist/ItemDetailC/ItemDetail.js
@@ -1,22 +1,26 @@
 import React from 'react'
+import {useState} from 'react'
 import { Card } from "react-bootstrap"
 import ItemCount from '../ItemCount'
+import {Link, NavLink} from 'react-router-dom'
 import './ItemDetail.css'
 
 const ItemDetail = ({producto}) => {
-    
+  const [show, setShow] =  useState(true)
+ const onAdd = (count) =>{
+   setShow(false)
+
+ }
     return (
         <div>
           <Card.Img className='imagenProd' variant="top" src={producto.img}  />
-             
-            
             {/* <Card.Title>{producto.nombre}</Card.Title> */}
             <Card.Title>{producto.descripcion}</Card.Title>
             <Card.Title>{producto.precio}</Card.Title>
-            <ItemCount minimo={1}  maximo={producto.stock} stock={producto.stock} />
-          <a href="/"><button className='bn632-hover bn25'>Agregar al carrito</button></a> 
-
-           
+           {show ? <ItemCount onAdd={onAdd} minimo={1}  maximo={producto.stock} stock={producto.stock} /> : 
+           <div> <Link to='/cart'><button>terminar la compra</button></Link>
+           <Link to='/'><button>seguir comprando</button></Link>
+          </div>}   
     </div>
     )
 }
