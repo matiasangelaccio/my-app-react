a5ddf343083705c2952cd91339fd0498c5f61e5a
matias <matiasangelaccio03@gmail.com>
2022-01-27 12:33:22 -0300

    entega firebase

--- src/componentes/itemlist/ItemDetailC/ItemDetailContainer.js
+++ src/componentes/itemlist/ItemDetailC/ItemDetailContainer.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import { getFetch } from '../Mock'
 import ItemDetail from '../ItemDetailC/ItemDetail'
 import { useParams } from 'react-router-dom'
+import { getFirestore, doc, getDoc } from 'firebase/firestore'
 const ItemDetailContainer = () => {
     const [producto, setProducto] = useState({})
     const [loading, setLoading] = useState(true);
     const { idDetalle } = useParams()
     useEffect(() => {
-        getFetch
-            .then(resp => {
-                setProducto(resp.find(prod => (prod.id) === idDetalle))
-                setLoading(false);
-            })
+        const db = getFirestore()
+        const queryProd = doc(db, 'items', idDetalle)
+        getDoc(queryProd)
+            .then(resp => setProducto({ id: resp.id, ...resp.data() }))
+            setLoading(false)
     }, [idDetalle])
 
     return (
--- src/componentes/itemlist/ItemListContainer.js
+++ src/componentes/itemlist/ItemListContainer.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import ItemList from './ItemList'
 import { useState, useEffect } from 'react'
-import { getFetch } from './Mock'
 import { useParams } from 'react-router-dom'
-import{getDocs, getFirestore, query} from 'firebase/firestore'
+import{getDocs, getFirestore, query, collection, where} from 'firebase/firestore'
 const ItemListContainer = ({ greeting }) => {
 
     const [productos, setProductos] = useState([])
@@ -11,10 +10,27 @@ const ItemListContainer = ({ greeting }) => {
 
     const { idCategoria } = useParams()
     useEffect(() => {
-const db = getFirestore()
-const queryCollection = query(collection(db, 'items'))
+if(idCategoria){
+    const db = getFirestore()
+    const queryCollection = query( collection (db, 'items'),
+    where('categoria', '==', idCategoria))
+    getDocs(queryCollection)
+    .then(res=>setProductos(res.docs.map(prod =>({id: prod.id, ...prod.data()}))))
+    .catch(err => err)
+    .finally(() => setLoading(false))
+}
+else{
+    const db = getFirestore()
+const queryCollection = query( collection (db, 'items'))
 getDocs(queryCollection)
-.then(res=>console.log(res))
+.then(res=>setProductos(res.docs.map(prod =>({id: prod.id, ...prod.data()}))))
+.catch(err => err)
+.finally(() => setLoading(false))
+}
+
+// const queryProd = doc(db, 'items')
+// getDoc(queryProd)
+// .then(resp =>setProductos({id: resp.id, ...resp.data()}))
         // if (idCategoria) {
         //     getFetch
         //         .then(resp => setProductos(resp.filter(prod => prod.categoria === idCategoria)))
